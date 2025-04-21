974c61009982eaa80955b2f440fa7bf5d12a19ea
yj <yj@desktop.lan>
2025-04-21 11:45:33 +0900

    solving as brute force

---
+++ slidingwindows/test.py
@@ -0,0 +1,39 @@
+import unittest
+from leetcode_2 import Solution
+
+class test(unittest.TestCase):
+    def setUp(self):
+        self.instance = Solution()
+    
+    def test1(self):
+        input: str = "abcabcbb"
+        ans = self.instance.lengthOfLongestSubstring(input)
+        self.assertEqual(ans, 3)
+    
+    def test2(self):
+        input: str = "bbbbb"
+        ans = self.instance.lengthOfLongestSubstring(input)
+        self.assertEqual(ans, 1)
+
+    def test3(self):
+        input: str = "pwwkew"
+        ans = self.instance.lengthOfLongestSubstring(input)
+        self.assertEqual(ans, 3)
+    
+    def test4(self):
+        input: str = "aab"
+        ans = self.instance.lengthOfLongestSubstring(input)
+        self.assertEqual(ans, 2)
+    
+    def test5(self):
+        input: str = "dvda"
+        ans = self.instance.lengthOfLongestSubstring(input)
+        self.assertEqual(ans, 3)
+    
+    def test6(self):
+        input: str = " "
+        ans = self.instance.lengthOfLongestSubstring(input)
+        self.assertEqual(ans, 1)
+
+if __name__ == "__main__":
+    unittest.main()
