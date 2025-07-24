90399c68d76486a5ba0a2b1f8d6d359acbe95e34
ron <timyjkim@MacBook-Air.local>
2025-07-24 23:00:53 +0900

    memo

--- memo.py
+++ memo.py
@@ -3,12 +3,20 @@ import random
 import heapq
 from collections import deque
 
-h = [6,3,2,1,5,7]
-q = deque(h)
-a = heapq.heapify(h)
+# ransomNote = "aab"
+# magazine = "baa"
+# d = {}
+# for w in ransomNote:
+#     if w in d.keys():
+#         d[w] = d[w]+1
+#     else:
+#         d.setdefault(w,1)
 
-print(q)
-print(h)
-q.popleft()
-print(q)
-print(h)
+# for w in magazine:
+#     if w in d.keys():
+#         if d[w] > 0:
+#             d[w] = d[w] - 1
+#         else:
+#             return False
+#     else:
+#         return False
